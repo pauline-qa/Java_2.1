9220294709b22601c547a2c221a668b7a423859c
Polina Novikova <novikovapauline@gmail.com>
2021-07-25 14:20:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 5000;
+        int miles_amount = ticket_price / 20;
+        System.out.println(miles_amount);
+    }
+}
