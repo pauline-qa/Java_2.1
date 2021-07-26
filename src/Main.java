cb982c4355e485e9fb7929d472ceed00c2f86939
Polina Novikova <novikovapauline@gmail.com>
2021-07-26 23:54:53 +0300

    Code reformated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 5000;
-        int miles_amount = ticket_price / 20;
+        int ticketPrice = 2000;
+        int miles_amount = ticketPrice / 20;
         System.out.println(miles_amount);
     }
 }
